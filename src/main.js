8687820a2b142f37fde58a0e020b3f7ae7659327
Jonathan <jopalafo@ucsc.edu>
2020-06-29 22:38:13 -0700

    commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 let config = {
     type: Phaser.CANVAS,
     width: 640,
-    height: 480
+    height: 480,
   }
-  let game = new Phaser.Game(config); 
+  let game = new Phaser.Game(config);
+
+
+ 
